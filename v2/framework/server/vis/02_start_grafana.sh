a7dcb23d6055d4db7fd13fcc7345d6f17c6454ac
J. Park <jpark@keti.re.kr>
2022-07-21 15:02:01 +0900

    snapshot

--- v2/framework/server/vis/02_start_grafana.sh
+++ v2/framework/server/vis/02_start_grafana.sh
@@ -1,1 +1,2 @@
-docker run -d -p 3000:3000 --name=grafana -v grafana-storage:/var/lib/grafana grafana/grafana-oss
+#docker run -d -p 3000:3000 --name=grafana -v grafana-storage:/var/lib/grafana grafana/grafana-oss
+docker-compose up -d
