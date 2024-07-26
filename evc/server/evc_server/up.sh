2b9a45a49e34131e6cd5795b4a834884c18fa669
jpark <jpark@keti.re.kr>
2024-07-26 11:58:02 +0900

    snapshot & refactoring

--- evc/server/evc_server/up.sh
+++ evc/server/evc_server/up.sh
@@ -1,3 +1,14 @@
-# docker-compose up
-#docker compose up -d
-docker compose up
+mkdir ~/.evc
+mkdir ~/.evc/grafana
+mkdir ~/.evc/mariadb
+mkdir ~/.evc/redis
+mkdir ~/.evc/redisearch
+# sudo chown -R $(whoami):$(whoami) ~/.evc/grafana
+# sudo chmod -R 755 ~/.evc/grafana
+
+echo "UID=$(id -u)" > .env
+echo "GID=$(id -g)" >> .env
+
+# docker-compose up # OLD
+# docker compose up -d # Run backgroud process
+docker compose up # Run foreground process
