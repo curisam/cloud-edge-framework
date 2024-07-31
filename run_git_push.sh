7a5ea9c1a3cca7969c1cbd15cafd3937dfcf70b1
jpark <jpark@keti.re.kr>
2024-07-31 16:25:01 +0900

    EVC MariaDB container

--- run_git_push.sh
+++ run_git_push.sh
@@ -3,6 +3,7 @@ git add .
 git status
 #git commit -m "Snapshot"
 #git commit -m "REDIS code update"
-git commit -m "MariaDB container code update"
+#git commit -m "MariaDB container code update"
+git commit -m "EVC MariaDB container"
 #git commit -m "Documentation update"
 git push
