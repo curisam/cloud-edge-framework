6cf83f41004783649dcd5587b5d8e5603cc5faad
jpark <jpark@keti.re.kr>
2024-08-12 15:02:09 +0900

    EVC Framework Snapshot

--- evc/server/ansible_server/publish-me/run_build.sh
+++ evc/server/ansible_server/publish-me/run_build.sh
@@ -2,4 +2,4 @@
 
 docker buildx create --use
 docker buildx ls
-docker buildx build --platform linux/amd64,linux/arm64 -f Dockerfile -t ketirepo/publish-me:latest --push .
+docker buildx build --platform linux/amd64,linux/arm64 -f Dockerfile -t evcrepo/publish-me:latest --push .
